9cbf639cfbfbece014a1fe57b813e8e564f4c83f
Simon Ha <simon.ha@kcl.ac.uk>
2023-10-05 16:15:01 +0100

    Finish frequency counter for all cases

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for item in items:
+        if str(item) in frequencies.keys():
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1  
     return frequencies
